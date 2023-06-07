0a921868393be12d56ec3c9827d7537baf02f670
Dan Davidík <danieldavidik@appio.dev>
2023-06-07 20:07:21 +0200

    Initial Commit

---
+++ ILikeCats/ILikeCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILikeCatsApp.swift
+//  ILikeCats
+//
+//  Created by Daniel Davidík on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ILikeCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
